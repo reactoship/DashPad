c9105d363af328e7172bbd7b02a94d1c9259fe8a
Barry Huang <Barry.huang93@gmail.com>
2019-10-22 16:11:18 -0400

    styled Widgets

---
+++ Styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet, TouchableWithoutFeedback } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flex: 1,
+    flexWrap: 'wrap',
+    color: 'white'
+  },
+  widget: {
+    color: 'white',
+    width: '50%',
+    height: '50%',
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  themedText: {
+    color: 'white',
+    fontFamily: 'HelveticaNeue-UltraLight',
+    fontSize: 100
+  }
+})
