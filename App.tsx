1e9c72504a4dbaac80014eb794d3d58f08460f1d
Barry Huang <Barry.huang93@gmail.com>
2019-10-22 15:17:39 -0400

    layout

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.widget}>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.widget}>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.widget}>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.widget}>Open up App.tsx to start working on your app!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    // display: 'flex',
+    flexDirection: 'row',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexWrap: 'wrap'
+    // backgroundColor: '#fff'
+    // alignItems: 'center',
+    // alignContent: 'stretch',
+    // justifyContent: 'center'
   },
-});
+  widget: {
+    // flex: 1,
+    width: '50%',
+    height: '50%',
+    backgroundColor: 'green'
+  }
+})
