ceaec760cca4f21e2e2ffe4679467a457e20938e
Barry Huang <Barry.huang93@gmail.com>
2019-10-25 16:20:56 -0400

    console logging events

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
 import styles from '../Styles'
+import * as calendar from 'expo-calendar'
+import * as Permissions from 'expo-permissions'
 
 const Calendar = () => {
+  const getCalendar = async () => {
+    await Permissions.askAsync('calendar')
+    const events = await calendar.getCalendarsAsync('event')
+    console.log(events)
+  }
+
+  useEffect(() => {
+    getCalendar()
+  }, [])
   return (
     <View style={styles.widget}>
       <Text style={styles.themedText}>This is a Calendar</Text>
