26cc0785292b3e1a2c425f8ef8a8414e27e1ce8e
Alan Tang <atang8013@gmail.com>
2019-10-25 17:10:29 -0400

    RNcalendar setup

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import styles from "../Styles";
+// import RNCalendarEvents from "react-native-calendar-events";
+import * as Calendar from "expo-calendar";
+import * as Permissions from "expo-permissions";
 
-const Calendar = () => {
+// const getAuth = () => {
+//   RNCalendarEvents.authorizationStatus()
+//     .then(status => {
+//       return status;
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// };
+
+const calendarWwidget = () => {
+  // const [authStatus, setAuth] = useState("");
+  // useEffect(() => {
+  //   setAuth(getAuth());
+  // }, []);
+
+  const calEvents = Calendar.getCalendarsAsync("Calendar.EntityTypes.EVENT");
   return (
     <View style={styles.widget}>
-      <Text style={styles.themedText}>This is a Calendar</Text>
+      <Text style={styles.themedText}>{calEvents}</Text>
     </View>
   );
 };
 
-export default Calendar;
+export default calendarWwidget;
