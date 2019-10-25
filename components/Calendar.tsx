18307afbc9339d134d07804785f059d11fbcf0ba
Alan Tang <atang8013@gmail.com>
2019-10-25 17:09:47 -0400

    RNcalendar events setup

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import styles from '../Styles'
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "../Styles";
 
 const Calendar = () => {
   return (
     <View style={styles.widget}>
       <Text style={styles.themedText}>This is a Calendar</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
