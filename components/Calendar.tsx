8b7fe01deff1629612125c947fb350aa28f76927
Alan Tang <atang8013@gmail.com>
2019-10-28 18:06:10 -0400

    retrieving upcoming events

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,24 +1,38 @@
-import React, { useEffect } from 'react'
-import { View, Text } from 'react-native'
-import styles from '../Styles'
-import * as calendar from 'expo-calendar'
-import * as Permissions from 'expo-permissions'
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import styles from "../Styles";
+import * as calendar from "expo-calendar";
+import * as Permissions from "expo-permissions";
+
+const getCalendar = async () => {
+  await Permissions.askAsync("calendar");
+  const events = await calendar.getCalendarsAsync("event");
+
+  const calendarIDs = [];
+  events.forEach(calendar => calendarIDs.push(calendar.id));
+
+  let day = new Date();
+  let fourDaysOut = new Date(day);
+  fourDaysOut.setDate(day.getDate() + 4);
+
+  const upcoming = await calendar.getEventsAsync(calendarIDs, day, fourDaysOut);
+
+  return upcoming;
+};
 
 const Calendar = () => {
-  const getCalendar = async () => {
-    await Permissions.askAsync('calendar')
-    const events = await calendar.getCalendarsAsync('event')
-    console.log(events)
-  }
+  const [calendar, grabEvents] = useState([]);
 
   useEffect(() => {
-    getCalendar()
-  }, [])
+    grabEvents(getCalendar());
+  }, []);
+  console.log(calendar, "$$$$$$$");
+
   return (
     <View style={styles.widget}>
       <Text style={styles.themedText}>This is a Calendar</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
